9027825c5798f6e5e61b5239e3195808c557f983
kkkipppo <museorange@naver.com>
2022-06-09 15:39:30 +0900

    first uplada

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+
+
+
+function mobile(){
+    $('nav').hide();
+    $('.btnNav').click(function(){
+        $('nav').show();
+    });
+
+    $('.btnClose').click(function(){
+        $('nav').hide();
+    });
+}//mobile
+
+
+function pc(){
+
+    $('nav').show();
+
+}//pc
+
+function common(){
+
+}//common
+
+
+function resp(){
+    let winWid = $(window).width();
+if(winWid < 1200){
+    mobile();
+}else{
+    pc();
+}
+}
+
+resp();
+$(window).resize(resp);
+
+common();
